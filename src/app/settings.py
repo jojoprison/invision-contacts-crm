af0f495ff5612e56fb11112878defcd3986adc87
jojoprison <egyabig2@gmail.com>
2025-07-01 01:00:52 +0700

    tenant middleware up

--- src/app/settings.py
+++ src/app/settings.py
@@ -34,12 +34,12 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'tenants.middleware.TenantMiddleware',  # Возвращаем кастомный middleware
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'tenants.middleware.TenantMiddleware',
 ]
 
 ROOT_URLCONF = 'app.urls'
