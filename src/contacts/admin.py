8e7c506f8f7fc978a39698c705bc1adbff2480ec
jojoprison <egyabig2@gmail.com>
2025-07-01 19:02:26 +0700

    added admin stuff

--- src/contacts/admin.py
+++ src/contacts/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import Contact
 
 # Register your models here.
+
+@admin.register(Contact)
+class ContactAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email', 'phone', 'date_created')
+    list_filter = ('date_created',)
+    search_fields = ('name', 'email', 'phone')
+    readonly_fields = ('id', 'date_created')
+    fieldsets = (
+        ('Основная информация', {
+            'fields': ('id', 'name', 'email')
+        }),
+        ('Дополнительная информация', {
+            'fields': ('phone', 'date_created')
+        }),
+    )
--- src/tenants/admin.py
+++ src/tenants/admin.py
@@ -1,3 +1,35 @@
 from django.contrib import admin
+from .models import Tenant, Domain
 
-# Register your models here.
+
+@admin.register(Tenant)
+class TenantAdmin(admin.ModelAdmin):
+    list_display = ('name', 'schema_name', 'created_at', 'auto_drop_schema')
+    list_filter = ('created_at', 'auto_drop_schema')
+    search_fields = ('name', 'schema_name')
+    readonly_fields = ('id', 'created_at')
+    fieldsets = (
+        ('Основная информация', {
+            'fields': ('id', 'name', 'schema_name')
+        }),
+        ('Настройки', {
+            'fields': ('auto_drop_schema', 'created_at')
+        }),
+    )
+
+
+@admin.register(Domain)
+class DomainAdmin(admin.ModelAdmin):
+    list_display = ('domain', 'tenant', 'is_primary', 'folder')
+    list_filter = ('is_primary', 'tenant')
+    search_fields = ('domain', 'folder')
+    readonly_fields = ('id',)
+    autocomplete_fields = ('tenant',)
+    fieldsets = (
+        ('Основная информация', {
+            'fields': ('id', 'domain', 'tenant')
+        }),
+        ('Настройки', {
+            'fields': ('is_primary', 'folder')
+        }),
+    )
